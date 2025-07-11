395a6274fa7a7634cac89d6c82aa94eab25db15c
drosproos <162022871+drosproos@users.noreply.github.com>
2025-07-11 21:48:20 +0500

    Update start.sh

--- server/start.sh
+++ server/start.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-echo "📦 Downloading PaperMC 1.21.1 Build 39..."
-curl -o paper.jar https://api.papermc.io/v2/projects/paper/versions/1.21.1/builds/39/downloads/paper-1.21.1-39.jar
+echo "📦 Checking for PaperMC..."
+if [ ! -f paper.jar ]; then
+  echo "📦 Downloading PaperMC 1.21.1 Build 39..."
+  curl -o paper.jar https://api.papermc.io/v2/projects/paper/versions/1.21.1/builds/39/downloads/paper-1.21.1-39.jar
+fi
 
 echo "✅ Accepted EULA"
 echo "eula=true" > eula.txt
 
 echo "🚀 Starting Minecraft Server..."
-java -Xmx1024M -Xms1024M -jar paper.jar nogui
+chmod +x paper.jar
+java -Xmx8192M -Xms8192M -jar paper.jar nogui
+
